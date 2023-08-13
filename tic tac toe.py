618c20e92bd4f1681bad95e1caf6f9b33d16d417
Sabbir12488 <sabbir12488@gmail.com>
2023-08-13 10:32:13 +0600

    added input

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,27 +1,44 @@
 import os
 
-colb = 20
-row = 6
+colb = 7
+row = 7
 
-count = 0
+values = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-#while True:
-for c in range(6):
-    print(' '*row + '|' + ' '*row + '|'+' '*row)
-    count += 1
-    if count == 2:
-        print('-'*colb)
-    if count == 4:
-        print('-'*colb)
-    
-command = input('press 1 through 9: ')
+X = 'X'
+O = 'O'
 
-if command == '3':
+def creat_grid():
+    for c in range(7):  
+        if c == 1:       
+            print(' '*3 + "{}".format(values[0])+ ' '*3+'|' + ' '*3 + "{}".format(values[1]) + ' '*3 + '|' +' '*3+ "{}".format(values[2]))
+        elif c == 3:
+            print(' '*3 + "{}".format(values[3])+ ' '*3+'|' + ' '*3 + "{}".format(values[4]) + ' '*3 + '|' +' '*3+ "{}".format(values[5]))
+        elif c == 5:
+            print(' '*3 + "{}".format(values[6])+ ' '*3+'|' + ' '*3 + "{}".format(values[7]) + ' '*3 + '|' +' '*3+ "{}".format(values[8]))
+        else:
+            print(' '*row + '|' + ' '*row + '|'+' '*row)
+        if c == 1 or c == 3:
+            print('_'*colb + '|' + '_'*colb + '|'+'_'*colb)
+
+creat_grid()
+
+command = int(input('press 1 through 9: '))
+
+if command:
+    values[command - 1] = X
     os.system('cls')
-    for c in range(6):
-        print(' '*row + '|' + ' '*row + '|'+ ' X ')
-        count += 1
-        if count == 2:
-            print('-'*colb)
-        if count == 4:
-            print('-'*colb)
+    creat_grid()
+else:
+    print('Wrong command')
+#
+#if command == '3':
+#    os.system('cls')
+#    for c in range(6):
+#        print(' '*row + '|' + ' '*row + '|'+ ' X ')
+#        count += 1
+#        if count == 2:
+#            print('-'*colb)
+#        if count == 4:
+#            print('-'*colb)
+#

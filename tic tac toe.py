1a86e614b545ff4328d6d21987733879f6323ce3
Sabbir12488 <sabbir12488@gmail.com>
2023-08-13 12:50:06 +0600

    added AI ve-1

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,44 +1,56 @@
 import os
-
-colb = 7
-row = 7
+import random
 
 values = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+command = 0
 
 X = 'X'
 O = 'O'
 
-def creat_grid():
-    for c in range(7):  
-        if c == 1:       
-            print(' '*3 + "{}".format(values[0])+ ' '*3+'|' + ' '*3 + "{}".format(values[1]) + ' '*3 + '|' +' '*3+ "{}".format(values[2]))
-        elif c == 3:
-            print(' '*3 + "{}".format(values[3])+ ' '*3+'|' + ' '*3 + "{}".format(values[4]) + ' '*3 + '|' +' '*3+ "{}".format(values[5]))
-        elif c == 5:
-            print(' '*3 + "{}".format(values[6])+ ' '*3+'|' + ' '*3 + "{}".format(values[7]) + ' '*3 + '|' +' '*3+ "{}".format(values[8]))
+class perameters(object):
+    grid_size = 7
+    spacing = grid_size - 4
+    def creat_grid():
+        for c in range(7):  
+            if c == 1:       
+                print(' '*perameters.spacing + "{}".format(values[0])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(values[1]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(values[2]))
+            elif c == 3:
+                print(' '*perameters.spacing + "{}".format(values[3])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(values[4]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(values[5]))
+            elif c == 5:
+                print(' '*perameters.spacing + "{}".format(values[6])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(values[7]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(values[8]))
+            else:
+                print(' '*perameters.grid_size + '|' + ' '*perameters.grid_size + '|'+' '*perameters.grid_size)
+            if c == 1 or c == 3:
+                print('_'*perameters.grid_size + '|' + '_'*perameters.grid_size + '|'+'_'*perameters.grid_size)
+
+
+perameters.creat_grid()
+
+
+def AI():
+    if values[2] == X:
+        if random.randint(1, 100) < 70 and values[4] == ' ':
+            values[4] = O
         else:
-            print(' '*row + '|' + ' '*row + '|'+' '*row)
-        if c == 1 or c == 3:
-            print('_'*colb + '|' + '_'*colb + '|'+'_'*colb)
+            values[3] = O
+        
 
-creat_grid()
+def Game():
 
-command = int(input('press 1 through 9: '))
+    while ' ' in values:
+        command = int(input('Press 1 through 9: '))
 
-if command:
-    values[command - 1] = X
-    os.system('cls')
-    creat_grid()
-else:
-    print('Wrong command')
-#
-#if command == '3':
-#    os.system('cls')
-#    for c in range(6):
-#        print(' '*row + '|' + ' '*row + '|'+ ' X ')
-#        count += 1
-#        if count == 2:
-#            print('-'*colb)
-#        if count == 4:
-#            print('-'*colb)
-#
+        if command and values[command - 1] == ' ':
+            values[command - 1] = X
+            ## AI
+            AI()
+
+            os.system('cls')
+            perameters.creat_grid()
+        else:
+            print('Wrong command!! Try again')
+
+Game()        
+
+

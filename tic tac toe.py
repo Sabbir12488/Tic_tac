fb6cf4585b5bf9ad26f04929eb14c67f5f11dd2c
Sabbir12488 <sabbir12488@gmail.com>
2023-08-15 00:20:56 +0600

    improvements

--- tic tac toe.py
+++ tic tac toe.py
@@ -6,16 +6,16 @@ class perameters(object):
     grid_size = 7
     spacing = grid_size - 4
     def creat_grid():
-        for c in range(7):  
-            if c == 1:       
+        for l in range(perameters.grid_size):  
+            if l == 1:       
                 print(' '*perameters.spacing + "{}".format(cells[0])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(cells[1]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(cells[2]))
-            elif c == 3:
+            elif l == 3:
                 print(' '*perameters.spacing + "{}".format(cells[3])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(cells[4]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(cells[5]))
-            elif c == 5:
+            elif l == 5:
                 print(' '*perameters.spacing + "{}".format(cells[6])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(cells[7]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(cells[8]))
             else:
                 print(' '*perameters.grid_size + '|' + ' '*perameters.grid_size + '|'+' '*perameters.grid_size)
-            if c == 1 or c == 3:
+            if l == 1 or l == 3:
                 print('_'*perameters.grid_size + '|' + '_'*perameters.grid_size + '|'+'_'*perameters.grid_size)
 
 cells = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
@@ -26,8 +26,8 @@ X = 'X'
 O = 'O'
 
 
-x_won = 'X WON!!!'
-o_won = 'O WON!!!'
+x_won = '\nX WON!!!'
+o_won = '\nO WON!!!'
 
 
 print('-----------------------------------------')
@@ -52,68 +52,68 @@ def game_over():
         exit()
 
 def game_state():
-        ## To check if X won
-        if set(cells[0:3]) == {X}:
-            print(x_won)  
-            game_over()
-        elif set(cells[3:6]) == {X}:
-            print(x_won)
-            game_over()
-        elif set(cells[6:9]) == {X}:
-            print(x_won)
-            game_over()
-        elif cells[0] == X and cells[3] == X and cells[6] == X:
-            print(x_won)
-            game_over()
-        elif cells[0] == X and cells[4] == X and cells[8] == X:
-            print(x_won)
-            game_over()
-        elif cells[0] == X and cells[4] == X and cells[8] == X:
-            print(x_won)
-            game_over()
-        elif cells[2] == X and cells[5] == X and cells[8] == X:
-            print(x_won)
-            game_over()
-        elif cells[2] == X and cells[4] == X and cells[6] == X:
-            print(x_won)
-            game_over()
-        elif cells[1] == X and cells[4] == X and cells[7] == X:
-            print(x_won)
-            game_over()  
+    ## To check if X won
+    if set(cells[0:3]) == {X}:
+        print(x_won)  
+        game_over()
+    elif set(cells[3:6]) == {X}:
+        print(x_won)
+        game_over()
+    elif set(cells[6:9]) == {X}:
+        print(x_won)
+        game_over()
+    elif cells[0] == X and cells[3] == X and cells[6] == X:
+        print(x_won)
+        game_over()
+    elif cells[0] == X and cells[4] == X and cells[8] == X:
+        print(x_won)
+        game_over()
+    elif cells[0] == X and cells[4] == X and cells[8] == X:
+        print(x_won)
+        game_over()
+    elif cells[2] == X and cells[5] == X and cells[8] == X:
+        print(x_won)
+        game_over()
+    elif cells[2] == X and cells[4] == X and cells[6] == X:
+        print(x_won)
+        game_over()
+    elif cells[1] == X and cells[4] == X and cells[7] == X:
+        print(x_won)
+        game_over()  
 
-        ## To check if O won
-        if set(cells[0:3]) == {O}:
-            print(o_won)
-            game_over()
-        elif set(cells[3:6]) == {O}:
-            print(o_won)
-            game_over()
-        elif set(cells[6:9]) == {O}:
-            print(o_won)
-            game_over()
-        elif cells[0] == O and cells[3] == O and cells[6] == O:
-            print(o_won)
-            game_over()
-        elif cells[0] == O and cells[4] == O and cells[8] == O:
-            print(o_won)
-            game_over()
-        elif cells[0] == O and cells[4] == O and cells[8] == O:
-            print(o_won)
-            game_over()
-        elif cells[2] == O and cells[5] == O and cells[8] == O:
-            print(o_won)
-            game_over()
-        elif cells[2] == O and cells[4] == O and cells[6] == O:
-            print(o_won)
-            game_over()
-        elif cells[1] == O and cells[4] == O and cells[7] == O:
-            print(o_won)
-            game_over()
+    ## To check if O won
+    if set(cells[0:3]) == {O}:
+        print(o_won)
+        game_over()
+    elif set(cells[3:6]) == {O}:
+        print(o_won)
+        game_over()
+    elif set(cells[6:9]) == {O}:
+        print(o_won)
+        game_over()
+    elif cells[0] == O and cells[3] == O and cells[6] == O:
+        print(o_won)
+        game_over()
+    elif cells[0] == O and cells[4] == O and cells[8] == O:
+        print(o_won)
+        game_over()
+    elif cells[0] == O and cells[4] == O and cells[8] == O:
+        print(o_won)
+        game_over()
+    elif cells[2] == O and cells[5] == O and cells[8] == O:
+        print(o_won)
+        game_over()
+    elif cells[2] == O and cells[4] == O and cells[6] == O:
+        print(o_won)
+        game_over()
+    elif cells[1] == O and cells[4] == O and cells[7] == O:
+        print(o_won)
+        game_over()
 
-        ## Check if its a draw
-        if cells.count(' ') == 1 and perameters.game_over == False:
-            print("Game is a draw!!")
-            game_over()
+    ## Check if its a draw
+    if cells.count(' ') == 0 and perameters.game_over == False:
+        print("\nGame is a draw!!")
+        game_over()
 
 def Game():
 

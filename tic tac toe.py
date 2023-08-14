905480dcdb5ff6ee0030c462828123ca052da186
Sabbir12488 <sabbir12488@gmail.com>
2023-08-14 14:09:06 +0600

    Quit, restart, bug
    Added quit and restart. fixed the turn problem, where giving wrong turn switched the player and fixed the error after giver wrong command

--- tic tac toe.py
+++ tic tac toe.py
@@ -18,7 +18,6 @@ class perameters(object):
             if c == 1 or c == 3:
                 print('_'*perameters.grid_size + '|' + '_'*perameters.grid_size + '|'+'_'*perameters.grid_size)
 
-
 cells = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 command = 0
@@ -26,113 +25,130 @@ command = 0
 X = 'X'
 O = 'O'
 
+
 x_won = 'X WON!!!'
 o_won = 'O WON!!!'
 
 
-
-
-perameters.creat_grid()
-
+print('-----------------------------------------')
+print('||            tic tac toe              ||')
+print('-----------------------------------------\n')
+input("Press Enter to continue...")
+os.system('cls')
 
 #def AI():
-#    if random.randint(1, 100) < 70 and values[4] == ' ':
-#        values[4] = O
-#    else:
-#        values[3] = O
-        
+
+
+def game_over():
+    perameters.game_over = True
+    restart = input('Want to play again?[y/n]: ')
+    if restart.lower().strip() == 'y':
+        for i in range(int(len(cells))):
+            cells[i] = ' '
+        perameters.game_over = False
+        os.system('cls')
+        Game()
+    else:
+        exit()
 
 def game_state():
-    ## to check if X won
-    if set(cells[0:3]) == {X}:
-        print(x_won)  
-        perameters.game_over = True    
-    elif set(cells[3:6]) == {X}:
-        print(x_won)
-        perameters.game_over = True
-    elif set(cells[6:9]) == {X}:
-        print(x_won)
-        perameters.game_over = True
-    elif cells[0] == X and cells[3] == X and cells[6] == X:
-        print(x_won)
-        perameters.game_over = True
-    elif cells[0] == X and cells[4] == X and cells[8] == X:
-        print(x_won)
-        perameters.game_over = True
-    elif cells[0] == X and cells[4] == X and cells[8] == X:
-        print(x_won)
-        perameters.game_over = True
-    elif cells[2] == X and cells[5] == X and cells[8] == X:
-        print(x_won)
-        perameters.game_over = True
-    elif cells[2] == X and cells[4] == X and cells[6] == X:
-        print(x_won)
-        perameters.game_over = True
-    elif cells[1] == X and cells[4] == X and cells[7] == X:
-        print(x_won)  
-        perameters.game_over = True
+        ## To check if X won
+        if set(cells[0:3]) == {X}:
+            print(x_won)  
+            game_over()
+        elif set(cells[3:6]) == {X}:
+            print(x_won)
+            game_over()
+        elif set(cells[6:9]) == {X}:
+            print(x_won)
+            game_over()
+        elif cells[0] == X and cells[3] == X and cells[6] == X:
+            print(x_won)
+            game_over()
+        elif cells[0] == X and cells[4] == X and cells[8] == X:
+            print(x_won)
+            game_over()
+        elif cells[0] == X and cells[4] == X and cells[8] == X:
+            print(x_won)
+            game_over()
+        elif cells[2] == X and cells[5] == X and cells[8] == X:
+            print(x_won)
+            game_over()
+        elif cells[2] == X and cells[4] == X and cells[6] == X:
+            print(x_won)
+            game_over()
+        elif cells[1] == X and cells[4] == X and cells[7] == X:
+            print(x_won)
+            game_over()  
 
-    ## To check if O won
-    if set(cells[0:3]) == {O}:
-        print(o_won)
-        perameters.game_over = True
-    elif set(cells[3:6]) == {O}:
-        print(o_won)
-        perameters.game_over = True
-    elif set(cells[6:9]) == {O}:
-        print(o_won)
-        perameters.game_over = True
-    elif cells[0] == O and cells[3] == O and cells[6] == O:
-        print(o_won)
-        perameters.game_over = True
-    elif cells[0] == O and cells[4] == O and cells[8] == O:
-        print(o_won)
-        perameters.game_over = True
-    elif cells[0] == O and cells[4] == O and cells[8] == O:
-        print(o_won)
-        perameters.game_over = True
-    elif cells[2] == O and cells[5] == O and cells[8] == O:
-        print(o_won)
-        perameters.game_over = True
-    elif cells[2] == O and cells[4] == O and cells[6] == O:
-        print(o_won)
-        perameters.game_over = True
-    elif cells[1] == O and cells[4] == O and cells[7] == O:
-        print(o_won)
-        perameters.game_over = True
+        ## To check if O won
+        if set(cells[0:3]) == {O}:
+            print(o_won)
+            game_over()
+        elif set(cells[3:6]) == {O}:
+            print(o_won)
+            game_over()
+        elif set(cells[6:9]) == {O}:
+            print(o_won)
+            game_over()
+        elif cells[0] == O and cells[3] == O and cells[6] == O:
+            print(o_won)
+            game_over()
+        elif cells[0] == O and cells[4] == O and cells[8] == O:
+            print(o_won)
+            game_over()
+        elif cells[0] == O and cells[4] == O and cells[8] == O:
+            print(o_won)
+            game_over()
+        elif cells[2] == O and cells[5] == O and cells[8] == O:
+            print(o_won)
+            game_over()
+        elif cells[2] == O and cells[4] == O and cells[6] == O:
+            print(o_won)
+            game_over()
+        elif cells[1] == O and cells[4] == O and cells[7] == O:
+            print(o_won)
+            game_over()
 
-    ##Check if its a draw
-    if cells.count(' ') == 1 and perameters.game_over == False:
-        print("Game is a draw!!")
-        perameters.game_over = True
-
+        ## Check if its a draw
+        if cells.count(' ') == 1 and perameters.game_over == False:
+            print("Game is a draw!!")
+            game_over()
 
 def Game():
+
+    perameters.creat_grid()
+
     turns = False
     while ' ' in cells and not perameters.game_over:
         if not turns:
-            command = int(input('Press 1 through 9 [ X ]: '))
+            command = input('Press 1 through 9 [ X ]: ').strip()
             turns = True
-            if command and cells[command - 1] == ' ':
-                cells[command - 1] = X
+            if command.strip().isnumeric() and cells[int(command[0].strip()) - 1] == ' ':
+                cells[int(command[0].strip()) - 1] = X
                 ## AI
                 os.system('cls')
                 perameters.creat_grid()
                 game_state()
+            elif command.lower() == 'quit':
+                game_over()
             else:
                 print('Wrong command!! Try again')
+                turns = False
         else:
-            command = int(input('Press 1 through 9 [ O ]: '))
+            command = input('Press 1 through 9 [ O ]: ')
             turns = False
-            if command and cells[command - 1] == ' ':
-                cells[command - 1] = O
+            if command.strip().isnumeric() and cells[int(command[0].strip()) - 1] == ' ':
+                cells[int(command[0].strip()) - 1] = O
                 ## AI
                 os.system('cls')
                 perameters.creat_grid()
                 game_state()
+            elif command.lower().strip() == 'quit':
+                game_over()
             else:
                 print('Wrong command!! Try again')
-
+                turns = True
+    
 Game()        
-
 

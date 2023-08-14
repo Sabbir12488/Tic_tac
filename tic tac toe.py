07421f53014999362c7eef49f9f09c75293bd08b
Sabbir12488 <sabbir12488@gmail.com>
2023-08-14 11:56:20 +0600

    Added draw

--- tic tac toe.py
+++ tic tac toe.py
@@ -8,18 +8,18 @@ class perameters(object):
     def creat_grid():
         for c in range(7):  
             if c == 1:       
-                print(' '*perameters.spacing + "{}".format(values[0])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(values[1]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(values[2]))
+                print(' '*perameters.spacing + "{}".format(cells[0])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(cells[1]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(cells[2]))
             elif c == 3:
-                print(' '*perameters.spacing + "{}".format(values[3])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(values[4]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(values[5]))
+                print(' '*perameters.spacing + "{}".format(cells[3])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(cells[4]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(cells[5]))
             elif c == 5:
-                print(' '*perameters.spacing + "{}".format(values[6])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(values[7]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(values[8]))
+                print(' '*perameters.spacing + "{}".format(cells[6])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(cells[7]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(cells[8]))
             else:
                 print(' '*perameters.grid_size + '|' + ' '*perameters.grid_size + '|'+' '*perameters.grid_size)
             if c == 1 or c == 3:
                 print('_'*perameters.grid_size + '|' + '_'*perameters.grid_size + '|'+'_'*perameters.grid_size)
 
 
-values = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+cells = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 command = 0
 
@@ -44,72 +44,77 @@ perameters.creat_grid()
 
 def game_state():
     ## to check if X won
-    if set(values[0:3]) == {X}:
+    if set(cells[0:3]) == {X}:
         print(x_won)  
         perameters.game_over = True    
-    elif set(values[3:6]) == {X}:
+    elif set(cells[3:6]) == {X}:
         print(x_won)
         perameters.game_over = True
-    elif set(values[6:9]) == {X}:
+    elif set(cells[6:9]) == {X}:
         print(x_won)
         perameters.game_over = True
-    elif values[0] == X and values[3] == X and values[6] == X:
+    elif cells[0] == X and cells[3] == X and cells[6] == X:
         print(x_won)
         perameters.game_over = True
-    elif values[0] == X and values[4] == X and values[8] == X:
+    elif cells[0] == X and cells[4] == X and cells[8] == X:
         print(x_won)
         perameters.game_over = True
-    elif values[0] == X and values[4] == X and values[8] == X:
+    elif cells[0] == X and cells[4] == X and cells[8] == X:
         print(x_won)
         perameters.game_over = True
-    elif values[2] == X and values[5] == X and values[8] == X:
+    elif cells[2] == X and cells[5] == X and cells[8] == X:
         print(x_won)
         perameters.game_over = True
-    elif values[2] == X and values[4] == X and values[6] == X:
+    elif cells[2] == X and cells[4] == X and cells[6] == X:
         print(x_won)
         perameters.game_over = True
-    elif values[1] == X and values[4] == X and values[7] == X:
+    elif cells[1] == X and cells[4] == X and cells[7] == X:
         print(x_won)  
         perameters.game_over = True
 
     ## To check if O won
-    if set(values[0:3]) == {O}:
+    if set(cells[0:3]) == {O}:
         print(o_won)
         perameters.game_over = True
-    elif set(values[3:6]) == {O}:
+    elif set(cells[3:6]) == {O}:
         print(o_won)
         perameters.game_over = True
-    elif set(values[6:9]) == {O}:
+    elif set(cells[6:9]) == {O}:
         print(o_won)
         perameters.game_over = True
-    elif values[0] == O and values[3] == O and values[6] == O:
+    elif cells[0] == O and cells[3] == O and cells[6] == O:
         print(o_won)
         perameters.game_over = True
-    elif values[0] == O and values[4] == O and values[8] == O:
+    elif cells[0] == O and cells[4] == O and cells[8] == O:
         print(o_won)
         perameters.game_over = True
-    elif values[0] == O and values[4] == O and values[8] == O:
+    elif cells[0] == O and cells[4] == O and cells[8] == O:
         print(o_won)
         perameters.game_over = True
-    elif values[2] == O and values[5] == O and values[8] == O:
+    elif cells[2] == O and cells[5] == O and cells[8] == O:
         print(o_won)
         perameters.game_over = True
-    elif values[2] == O and values[4] == O and values[6] == O:
+    elif cells[2] == O and cells[4] == O and cells[6] == O:
         print(o_won)
         perameters.game_over = True
-    elif values[1] == O and values[4] == O and values[7] == O:
+    elif cells[1] == O and cells[4] == O and cells[7] == O:
         print(o_won)
+        perameters.game_over = True
+
+    ##Check if its a draw
+    if cells.count(' ') == 1 and perameters.game_over == False:
+        print("Game is a draw!!")
         perameters.game_over = True
 
 
 def Game():
     turns = False
-    while ' ' in values and not perameters.game_over:
+    while ' ' in cells and not perameters.game_over:
         if not turns:
             command = int(input('Press 1 through 9 [ X ]: '))
             turns = True
-            if command and values[command - 1] == ' ':
-                values[command - 1] = X
+            if command and cells[command - 1] == ' ':
+                cells[command - 1] = X
                 ## AI
                 os.system('cls')
                 perameters.creat_grid()
@@ -119,8 +124,8 @@ def Game():
         else:
             command = int(input('Press 1 through 9 [ O ]: '))
             turns = False
-            if command and values[command - 1] == ' ':
-                values[command - 1] = O
+            if command and cells[command - 1] == ' ':
+                cells[command - 1] = O
                 ## AI
                 os.system('cls')
                 perameters.creat_grid()

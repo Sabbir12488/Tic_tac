81da33650346ea5be98715a8aad1caddc9438862
Sabbir12488 <sabbir12488@gmail.com>
2023-08-17 15:01:10 +0600

    Added player name
    Added player name and display name. Cleaned up the grid and other functions

--- tic tac toe.py
+++ tic tac toe.py
@@ -6,6 +6,10 @@ class perameters(object):
     grid_size = 7
     spacing = grid_size - 4
     def creat_grid():
+        print('-----------------------')
+        print('{}    [X] | [O]    {}'.format(p_1_d, p_2_d))
+        print('-----------------------\n')
+
         for l in range(perameters.grid_size):  
             if l == 1:       
                 print(' '*perameters.spacing + "{}".format(cells[0])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(cells[1]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(cells[2]))
@@ -13,10 +17,12 @@ class perameters(object):
                 print(' '*perameters.spacing + "{}".format(cells[3])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(cells[4]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(cells[5]))
             elif l == 5:
                 print(' '*perameters.spacing + "{}".format(cells[6])+ ' '*perameters.spacing +'|' + ' '*perameters.spacing + "{}".format(cells[7]) + ' '*perameters.spacing + '|' + ' '*perameters.spacing + "{}".format(cells[8]))
+            elif l == 6:
+                print(' '*perameters.grid_size + '|' + ' '*perameters.grid_size + '|'+' '*perameters.grid_size + '\n')
             else:
                 print(' '*perameters.grid_size + '|' + ' '*perameters.grid_size + '|'+' '*perameters.grid_size)
             if l == 1 or l == 3:
-                print('_'*perameters.grid_size + '|' + '_'*perameters.grid_size + '|'+'_'*perameters.grid_size)
+                print('_'*perameters.grid_size + '|' + '_'*perameters.grid_size + '|'+'_'*perameters.grid_size)          
 
 cells = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
@@ -26,14 +32,40 @@ X = 'X'
 O = 'O'
 
 
-x_won = '\nX WON!!!'
-o_won = '\nO WON!!!'
+x_won = ''
+o_won = ''
 
-
+os.system('cls')
 print('-----------------------------------------')
 print('||            tic tac toe              ||')
 print('-----------------------------------------\n')
 input("Press Enter to continue...")
+os.system('cls')
+
+
+## Player name
+player_1 = input("Name (player_1): ")
+player_2 = input("Name (player_2): ")
+
+p_1_d = player_1.upper()[:3]
+p_2_d = player_2.upper()[:3]
+
+if len(player_1) != 0 and len(player_1) < 3:
+    p_1_d = player_1.upper() + ' '.ljust(3 - len(player_1))
+elif player_1.strip() == "":
+    p_1_d = 'P_1'
+    player_1 = 'P_1'
+
+if len(player_2) != 0 and len(player_2) < 3:
+    p_2_d = ' '.ljust(3 - len(player_2)) + player_2.upper()
+elif player_2.strip() == "":
+    p_2_d = 'P_2'
+    player_2 = 'P_2'
+
+    
+
+x_won = str(player_1.strip()) + ' won\n'
+o_won = str(player_2.strip()) + ' won\n'
 os.system('cls')
 
 #def AI():
@@ -122,7 +154,7 @@ def Game():
     turns = False
     while ' ' in cells and not perameters.game_over:
         if not turns:
-            command = input('Press 1 through 9 [ X ]: ').strip()
+            command = input('Press 1 through 9 [{}]: '.format(p_1_d)).strip()
             turns = True
             if command.strip().isnumeric() and cells[int(command[0].strip()) - 1] == ' ':
                 cells[int(command[0].strip()) - 1] = X
@@ -131,12 +163,12 @@ def Game():
                 perameters.creat_grid()
                 game_state()
             elif command.lower() == 'quit':
-                game_over()
+                exit()
             else:
                 print('Wrong command!! Try again')
                 turns = False
         else:
-            command = input('Press 1 through 9 [ O ]: ')
+            command = input('Press 1 through 9 [{}]: '.format(p_2_d)).strip()
             turns = False
             if command.strip().isnumeric() and cells[int(command[0].strip()) - 1] == ' ':
                 cells[int(command[0].strip()) - 1] = O
@@ -145,7 +177,7 @@ def Game():
                 perameters.creat_grid()
                 game_state()
             elif command.lower().strip() == 'quit':
-                game_over()
+                exit()
             else:
                 print('Wrong command!! Try again')
                 turns = True
